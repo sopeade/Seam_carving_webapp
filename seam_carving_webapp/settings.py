7ed357d5bbb427ca48cab643b7b34d111d620558
sopeade <lanlehinsope@gmail.com>
2023-10-23 23:52:48 -0500

    added Procfile

--- seam_carving_webapp/settings.py
+++ seam_carving_webapp/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # static main directory routing 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # celery configuration
 CELERY_BROKER_URL = "redis://localhost/0"
