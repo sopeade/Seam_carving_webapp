56a2e15321b35e135512dcc6d9ee4aa7b8bf3b1b
sopeade <lanlehinsope@gmail.com>
2023-10-24 17:07:46 -0500

    removed database sslmode

--- seam_carving_webapp/settings.py
+++ seam_carving_webapp/settings.py
@@ -156,3 +156,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 django_heroku.settings(locals())
+del DATABASES['default']['OPTIONS']['sslmode']
