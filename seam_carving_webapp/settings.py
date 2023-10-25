0ef234f10f86022ae5a29e65dced1458d0919408
sopeade <lanlehinsope@gmail.com>
2023-10-25 15:48:16 -0500

    updated settings

--- seam_carving_webapp/settings.py
+++ seam_carving_webapp/settings.py
@@ -156,4 +156,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 django_heroku.settings(locals())
-del DATABASES['default']['OPTIONS']['sslmode']
+# del DATABASES['default']['OPTIONS']['sslmode']
