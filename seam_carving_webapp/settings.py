b87c6dfb57da0efbb9cc297b908d16605cac928a
sopeade <lanlehinsope@gmail.com>
2023-10-27 02:37:41 -0500

    delete initial images

--- seam_carving_webapp/settings.py
+++ seam_carving_webapp/settings.py
@@ -165,4 +165,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 django_heroku.settings(locals())
-# del DATABASES['default']['OPTIONS']['sslmode']
+
+if DATABASES['default'].get(['OPTIONS']):
+    del DATABASES['default']['OPTIONS']['sslmode']
